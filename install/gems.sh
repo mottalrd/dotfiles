b49c36090006731447eabe99eb44b5960bbfd590
Alfredo Motta <motta@Alfredos-MacBook-Pro-4.local>
2025-04-07 10:53:34 +0200

    remove git personal details, better name for private config and cleanup of not used gems

--- install/gems.sh
+++ install/gems.sh
@@ -2,5 +2,4 @@ git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 gem install bundler
-gem install git-whistles
 gem install ripper-tags
